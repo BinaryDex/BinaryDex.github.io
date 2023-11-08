142f9960a6ec7d2b743ae1d3044aac2ebcaf630a
Dex <82985536+BinaryDex@users.noreply.github.com>
2023-11-07 20:01:41 -0500

    Update audio.js

--- script/audio.js
+++ script/audio.js
@@ -1,27 +1,27 @@
-var audio = document.getElementById('audio')
-var init = false
+var audio = document.getElementById('audio');
+var init = false;
 
-audio.volume = 0.085
+audio.volume = 0.085;
 
 function start() {
-    if (init) return
-    else init = true
+    if (init) return;
+    else init = true;
     
-    var rythm = new Rythm()
-    rythm.addRythm('img', 'image-shadow', 0, 10)
-    rythm.addRythm('title', 'text-shadow', 0, 10)
-    rythm.addRythm('text', 'text-shadow', 0, 10)
-    rythm.addRythm('index_message', 'text-shadow', 0, 10)
-    rythm.connectExternalAudioElement(audio)
-    rythm.start()
+    var rythm = new Rythm();
+    rythm.addRythm('img', 'image-shadow', 0, 10);
+    rythm.addRythm('title', 'text-shadow', 0, 10);
+    rythm.addRythm('text', 'text-shadow', 0, 10);
+    rythm.addRythm('index_message', 'text-shadow', 0, 10);
+    rythm.connectExternalAudioElement(audio);
+    rythm.start();
 }
 
-setInterval(() => {
+document.getElementsByName('container_init')[0].addEventListener('click', function() {
     audio.play()
-    .then(() => {
-        start()
-        document.getElementsByName('container_init')[0].style.visibility = 'hidden'
-        document.getElementsByName('container_main')[0].style.visibility = 'visible'
-    })
-    .catch(err => {})
-}, 100)
+        .then(() => {
+            start();
+            document.getElementsByName('container_init')[0].style.visibility = 'hidden';
+            document.getElementsByName('container_main')[0].style.visibility = 'visible';
+        })
+        .catch(err => {});
+});
